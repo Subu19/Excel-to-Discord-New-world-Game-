5677d3a005926f5d66e70e8e955d65f729ece020
Subu <59548115+Subu19@users.noreply.github.com>
2023-07-28 16:28:41 +0545

    fix crashes

--- index.js
+++ index.js
@@ -42,12 +42,14 @@ async function fetchPlayers() {
   const data = await readExcel("./test.xlsx");
   const columnData = await generateProperJSON(data);
   const embed = new Discord.EmbedBuilder();
-  embed.setTitle("KLESS HIVE ROSTER");
+  embed.setTitle("Reckless Hive Roster");
   embed.setColor("Random");
   embed.setFooter({
-    text: "⚔️ Assassin (Melee), 🏹 Assassin (Ranged), 🟠 Bruiser, 🔵 DPS: Blunderbuss/Ice, 🟡 DPS: Fire staff, 🟢 Healer, 🟣 Support, 🦾 Tank",
+    text: "🔴Assassin (Melee), 🏹Assassin (Ranged), 🟠Bruiser, ⚪DPS:Blunderbuss/Ice-Gauntlet \n🔵DPS:Fire-staff, 🟢Healer, 🟣VG-IG, 🟡Tank, 🟤Spear-IG",
   });
-  embed.setImage("https://i.ytimg.com/vi/oK2qg_OjqD8/maxresdefault.jpg");
+  embed.setImage(
+    "https://media.discordapp.net/attachments/725712044501893150/1134428956091748465/logo.png?width=570&height=663"
+  );
   for (let i = 0; i < columnData.length; i++) {
     const element = columnData[i];
     var players = [];
@@ -83,11 +85,21 @@ async function fetchPlayers() {
     }
   }
   if (config.last_message) {
-    client.channels.fetch(config.channel).then(async (channel) => {
-      await channel.messages.fetch(config.last_message).then((msg) => {
-        msg.edit({ embeds: [embed] });
+    client.channels
+      .fetch(config.channel)
+      .then(async (channel) => {
+        await channel.messages
+          .fetch(config.last_message)
+          .then((msg) => {
+            msg.edit({ embeds: [embed] });
+          })
+          .catch((err) => {
+            clearConfig();
+          });
+      })
+      .catch(() => {
+        clearConfig();
       });
-    });
   } else {
     await client.guilds.cache
       .get(process.env.GUILD)
@@ -107,6 +119,20 @@ async function fetchPlayers() {
   }
 }
 
+//clear config
+
+function clearConfig() {
+  var newConfig = config;
+  newConfig.channel = "";
+  newConfig.last_message = "";
+  fs.writeFile("./config.json", JSON.stringify(newConfig, null, 2), (error) => {
+    if (err) {
+      console.log("error while saving");
+    } else {
+      console.log("saved!");
+    }
+  });
+}
 // Function to read Excel data
 async function readExcel(filePath) {
   return new Promise(async (resolve, reject) => {
@@ -119,7 +145,7 @@ async function readExcel(filePath) {
           type: "buffer",
         });
         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        resolve(xlsx.utils.sheet_to_json(worksheet));
+        resolve(xlsx.utils.sheet_to_json(worksheet, { blankrows: true }));
       })
       .catch((err) => {
         console.log(err);
@@ -150,126 +176,130 @@ async function generateProperJSON(data) {
 }
 
 ////////REPLACE ROLENAME
-
 function replaceRoleName(role) {
   return role
-    .replace("Assassin (Melee)", "⚔️")
+    .replace("Assassin (Melee)", "🔴")
     .replace("Assassin (Ranged)", "🏹")
     .replace("Bruiser", "🟠")
-    .replace("DPS: Blunderbuss/Ice", "🔵")
-    .replace("DPS: Fire staff", "🟡")
+    .replace("DPS: Blunderbuss/Ice", "⚪")
+    .replace("DPS: Fire staff", "🔵")
     .replace("Healer", "🟢")
-    .replace("Support", "🟣")
-    .replace("Tank", "🦾");
+    .replace("VG IG", "🟣")
+    .replace("Tank", "🟡")
+    .replace("Spear IG", "🟤");
 }
 
 // Command to import and send Excel data to Discord
 client.on("messageCreate", async (message) => {
   /////////import command
   if (message.content === "n!import") {
-    if (!message.member.permissionsIn(message.channel).has("Administrator"))
-      return message.channel.send("You are not an admin");
-    const data = await readExcel("./test.xlsx");
-    const columnData = await generateProperJSON(data);
-    const embed = new Discord.EmbedBuilder();
-    embed.setTitle("KLESS HIVE ROSTER");
-    embed.setColor("Random");
-    embed.setFooter({
-      text: "⚔️ Assassin (Melee), 🏹 Assassin (Ranged), 🟠 Bruiser, 🔵 DPS: Blunderbuss/Ice, 🟡 DPS: Fire staff, 🟢 Healer, 🟣 Support, 🦾 Tank",
-    });
-    for (let i = 0; i < columnData.length; i++) {
-      const element = columnData[i];
-      var players = [];
-      await element.players.forEach((player, i) => {
-        if (
-          client.guilds.cache
-            .get(process.env.GUILD)
-            .members.cache.find(
-              (user) =>
-                user.nickname == player.name || user.displayName == player.name
-            )
-        ) {
-          const user = client.guilds.cache
-            .get(process.env.GUILD)
-            .members.cache.find(
-              (user) =>
-                user.nickname == player.name || user.displayName == player.name
-            );
-          players.push(replaceRoleName(player.role) + " <@" + user.id + ">");
-        } else {
-          players.push(replaceRoleName(player.role) + " " + player.name);
-        }
+    try {
+      if (!message.member.permissionsIn(message.channel).has("Administrator"))
+        return message.channel.send("You are not an admin");
+      const data = await readExcel("./test.xlsx");
+      const columnData = await generateProperJSON(data);
+      const embed = new Discord.EmbedBuilder();
+      embed.setTitle("Reckless Hive Roster");
+      embed.setColor("Random");
+      embed.setFooter({
+        text: "🔴Assassin (Melee), 🏹Assassin (Ranged), 🟠Bruiser, ⚪DPS: Blunderbuss/Ice, 🔵DPS: Fire-staff, 🟢Healer, 🟣VG-IG, 🟡Tank, 🟤Spear-IG",
       });
+      for (let i = 0; i < columnData.length; i++) {
+        const element = columnData[i];
+        var players = [];
+        await element.players.forEach((player, i) => {
+          if (
+            client.guilds.cache
+              .get(process.env.GUILD)
+              .members.cache.find(
+                (user) =>
+                  user.nickname == player.name ||
+                  user.displayName == player.name
+              )
+          ) {
+            const user = client.guilds.cache
+              .get(process.env.GUILD)
+              .members.cache.find(
+                (user) =>
+                  user.nickname == player.name ||
+                  user.displayName == player.name
+              );
+            players.push(replaceRoleName(player.role) + " <@" + user.id + ">");
+          } else {
+            players.push(replaceRoleName(player.role) + " " + player.name);
+          }
+        });
 
-      embed.addFields({
-        name: element.name,
-        value: players.join(" \n"),
-        inline: true,
-      });
-      if ((i + 1) % 3 == 0) {
-        embed.addFields({ name: "\u200b", value: "\u200b" });
+        embed.addFields({
+          name: element.name,
+          value: players.join(" \n"),
+          inline: true,
+        });
+        if ((i + 1) % 3 == 0) {
+          embed.addFields({ name: "\u200b", value: "\u200b" });
+        }
       }
+      await message.channel.send({ embeds: [embed] });
+    } catch (error) {
+      console.log(error);
     }
-    await message.channel.send({ embeds: [embed] });
   }
 
   ///////////////////////// update command//////////////////////
   if (message.content == "n!updatechannel" && !message.author.bot) {
-    if (!message.member.permissionsIn(message.channel).has("Administrator"))
-      return message.channel.send("You are not an admin");
-    message.channel.send("Done").then((msg) => {
-      setTimeout(() => {
-        msg.delete();
-        message.delete();
-      }, 5000);
-    });
-    var newConfig = config;
-    newConfig.channel = message.channel.id;
-    newConfig.last_message = "";
-    fs.writeFile(
-      "./config.json",
-      JSON.stringify(newConfig, null, 2),
-      (error) => {
-        if (err) {
-          console.log("error while saving");
-        } else {
-          console.log("saved!");
+    try {
+      if (!message.member.permissionsIn(message.channel).has("Administrator"))
+        return message.channel.send("You are not an admin");
+      message.channel.send("Done").then((msg) => {
+        setTimeout(() => {
+          msg.delete();
+          message.delete();
+        }, 5000);
+      });
+      var newConfig = config;
+      newConfig.channel = message.channel.id;
+      newConfig.last_message = "";
+      fs.writeFile(
+        "./config.json",
+        JSON.stringify(newConfig, null, 2),
+        (error) => {
+          if (err) {
+            console.log("error while saving");
+          } else {
+            console.log("saved!");
+          }
         }
-      }
-    );
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   //////////////////////stop command.//////////////////////////
   if (message.content == "n!stopupdate") {
-    if (!message.member.permissionsIn(message.channel).has("Administrator"))
-      return message.channel.send("You are not an admin");
-    var newConfig = config;
-    newConfig.channel = "";
-    newConfig.last_message = "";
-    fs.writeFile(
-      "./config.json",
-      JSON.stringify(newConfig, null, 2),
-      (error) => {
-        if (err) {
-          console.log("error while saving");
-        } else {
-          console.log("saved!");
-          message.channel.send("Stopped updating");
-        }
-      }
-    );
+    try {
+      if (!message.member.permissionsIn(message.channel).has("Administrator"))
+        return message.channel.send("You are not an admin");
+      clearConfig();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   ////////////////////help command ////////////////////////////
   if (message.content == "n!help") {
-    const embed = new Discord.EmbedBuilder();
-    embed.setTitle("My commands are:");
-    embed.addFields({ name: "n!stopupdate", value: "Stop updating roles" });
-    embed.addFields({
-      name: "n!updatechannel",
-      value: "Set channel to send updates",
-    });
-    embed.addFields({ name: "n!import", value: "Sends you role embed" });
-    message.channel.send({ embeds: [embed] });
+    try {
+      const embed = new Discord.EmbedBuilder();
+      embed.setTitle("My commands are:");
+      embed.addFields({ name: "n!stopupdate", value: "Stop updating roles" });
+      embed.addFields({
+        name: "n!updatechannel",
+        value: "Set channel to send updates",
+      });
+      embed.addFields({ name: "n!import", value: "Sends you role embed" });
+      message.channel.send({ embeds: [embed] });
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
